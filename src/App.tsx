f20de810064c8ce9681b492ea5e60a09776d8542
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2025-05-18 19:41:25 -0400

    Ready for Publish

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import "./MainCard.tsx"
-import MainCard from './MainCard.tsx'
-import documentLogo from './assets/document.svg';
+import "./MainPage.tsx"
+import MainPage from './MainPage.tsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div> */}
-      <h1>
-        Nano Technology Engineering Class Backup
-      </h1>
-      <h2>
-        Class Notes and Resources Hosted by MrDNAlex
-      </h2>
-      
-      {/* <h1>Vite + React</h1> */}
-      {/* <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-
-        <p>
-          Hello from Vite and React in HTML
-        </p>
-      </div> */}
-
-      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", width: "100%" }}>
-        <MainCard />
-      </div>
+      <h1> Nano Technology Engineering Class Backups </h1>
+      <h2> Class Notes and Resources Hosted by MrDNAlex </h2>
+      <MainPage />
     </>
   )
 }
--- src/FolderItem.tsx
+++ src/FolderItem.tsx
@@ -4,7 +4,6 @@ import downloadLogo from './assets/download.svg'
 import folder from './assets/folder.svg'
 import document from './assets/document.svg'
 
-
 interface FolderItemProps {
     IsFile: boolean;
     Name: string;
@@ -13,39 +12,14 @@ interface FolderItemProps {
     onClick: () => void;
 }
 
-
 class FolderItem extends React.Component<FolderItemProps> {
 
     constructor(props: FolderItemProps) {
         super(props);
     }
 
-    GetTags() {
-        let tags: string[] = this.props.Tags;
-
-        return (
-            <div style={{ flex: 4, flexDirection: "row", display: "flex", flexWrap: "nowrap", gap: "5px" }}>
-
-                {tags.map((item, index) => (<p className="folder-item-property" key={index} style={{ borderRadius: "30%", backgroundColor: "red", padding: "5px" }} >
-                    {item}
-                </p>))}
-            </div>
-        );
-    }
-
     render() {
-
-        let imageFile;
-        //let downloadable;
-
-        if (this.props.IsFile) {
-            imageFile = document;
-            //downloadable = { visibility: "visible" };
-        } else {
-            imageFile = folder;
-            //downloadable = { visibility: "hidden" };
-        }
-
+        let imageFile: string = this.props.IsFile ? document : folder;
         let downloadable: "visible" | "hidden" = this.props.IsFile ? "visible" : "hidden";
 
         return (
@@ -54,12 +28,9 @@ class FolderItem extends React.Component<FolderItemProps> {
                     <img style={{ width: "60%" }} src={imageFile} alt="Folder Icon" />
                 </div>
                 <p className="folder-item-property" style={{ flex: 4 }}> {this.props.Name} </p>
-                {this.GetTags()}
-                {/* <p className="folder-item-property" style={{ flex: 2 }}> {this.props.Professor} </p>
-                <p className="folder-item-property" style={{ flex: 1 }}> {this.props.Term} </p> */}
                 <p className="folder-item-property" style={{ flex: 1 }}> {this.props.Size} </p>
                 <div style={{ flex: 1, }}>
-                    <img style={{ width: "60%", color: "blue", stroke: "blue", fill: "blue", visibility:downloadable }} src={downloadLogo} alt="Download Icon" />
+                    <img style={{ width: "60%", color: "blue", stroke: "blue", fill: "blue", visibility: downloadable }} src={downloadLogo} alt="Download Icon" />
                 </div>
             </div>
         );
